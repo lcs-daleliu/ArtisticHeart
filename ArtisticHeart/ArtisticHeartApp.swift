889be3b49729d2f33506f271415ef668785db8e9
Dale Liu <YLiu@lcs.on.ca>
2024-05-24 11:29:11 -0400

    Initial Commit

---
+++ ArtisticHeart/ArtisticHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtisticHeartApp.swift
+//  ArtisticHeart
+//
+//  Created by Yiren LIU on 2024-05-24.
+//
+
+import SwiftUI
+
+@main
+struct ArtisticHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
